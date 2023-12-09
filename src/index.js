a6c88a66532ce7a5f4232557098be05f37eb7de5
SharjeelAM <zhac221@live.rhul.ac.uk>
2023-12-09 01:23:23 +0000

    This is the initial upload of my FYP. It includes a functional signup/login page and a functional offline prototype of the snakes and ladders board game.

---
+++ src/index.js
@@ -0,0 +1,73 @@
+const express = require("express")
+const path = require("path")
+const app = express()
+const hbs = require("hbs")
+const LogInCollection = require("./mongodb")
+const port = process.env.PORT || 3000
+app.use(express.json())
+
+app.use(express.urlencoded({ extended: false }))
+
+const templatePath = path.join(__dirname, '../templates')
+const publicPath = path.join(__dirname, '../public')
+console.log(publicPath);
+
+app.set('view engine', 'hbs')
+app.set('views', templatePath)
+app.use(express.static(publicPath))
+
+
+app.get('/signup', (req, res) => {
+    res.render('signup')
+})
+app.get('/', (req, res) => {
+    res.render('login')
+})
+
+app.post('/signup', async (req, res) => {
+
+    const data = {
+        name: req.body.name,
+        password: req.body.password
+    }
+    
+    await LogInCollection.insertMany([data])
+    
+    res.render("home")
+        
+
+
+})
+
+
+app.post('/login', async (req, res) => {
+
+    try {
+        const check = await LogInCollection.findOne({ name: req.body.name })
+
+        if (check.password === req.body.password) {
+            res.status(201).render("home")
+        }
+
+        else {
+            res.send("incorrect password")
+        }
+
+
+    } 
+    
+    catch (e) {
+
+        res.send("wrong details")
+        
+
+    }
+
+
+})
+
+
+
+app.listen(port, () => {
+    console.log('port connected');
+})
